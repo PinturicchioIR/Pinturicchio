c894811a7165f3f5bc9e40a0189169e27bad52e2
Ali Daneshamnd <49141347+MrAnderss0n@users.noreply.github.com>
2022-05-20 23:34:27 +0430

    Upgrade technologies versions and Laravel to v9 (#17)
    * Add platform branch (#15)
    
    * feat: Add dcoker compose and its related files (#3)
    
    Co-authored-by: MrAnderss0n <adaneshmandm@gmail.com>
    
    * chore: Update to Laravel 7.7.1
    
    * Laravel 7.12, Redis 6 and Nginx new config (#6)
    
    * Update nginx configuration
    
    * Use redis v6
    
    * Update laravel to 7.12
    
    * Update to Laravel 7.14.1
    
    * Upgrade to Laravel v8
    
    * Add platform branch to the project
    
    * Delete composer lock file
    
    Co-authored-by: Pinturicchio <62214903+PinturicchioIR@users.noreply.github.com>
    
    * Upgrade technologies versions
    
    * Upgrade to Laravel v9
    
    * Clean-up for Laravel v9
    
    * Minor changes over docker-compose file ports
    
    Co-authored-by: Pinturicchio <62214903+PinturicchioIR@users.noreply.github.com>

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,7 @@ class User extends AuthenticatableModelBase
     /**
      * The attributes that are mass assignable.
      *
-     * @var string[]
+     * @var array<int, string>
      */
     protected $fillable = [
         'name',
@@ -20,7 +20,7 @@ class User extends AuthenticatableModelBase
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
@@ -30,7 +30,7 @@ class User extends AuthenticatableModelBase
     /**
      * The attributes that should be cast.
      *
-     * @var array
+     * @var array<string, string>
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
