865c4c14f4ddf1e7d68e1ced0162df856f37d5a9
Anderss0n <adaneshmandm@gmail.com>
2022-05-20 21:45:37 +0430

    Upgrade to Laravel v9

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,20 @@
 
 namespace App\Models;
 
-use Platform\Bases\AuthenticatableModelBase;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends AuthenticatableModelBase
+class User extends Authenticatable
 {
+    use HasApiTokens, HasFactory, Notifiable;
+
     /**
      * The attributes that are mass assignable.
      *
-     * @var string[]
+     * @var array<int, string>
      */
     protected $fillable = [
         'name',
@@ -20,7 +26,7 @@ class User extends AuthenticatableModelBase
     /**
      * The attributes that should be hidden for serialization.
      *
-     * @var array
+     * @var array<int, string>
      */
     protected $hidden = [
         'password',
@@ -30,7 +36,7 @@ class User extends AuthenticatableModelBase
     /**
      * The attributes that should be cast.
      *
-     * @var array
+     * @var array<string, string>
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
